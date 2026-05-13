5bf219ffd38e4c42638034fd10c398542f36f797
Dariush Tahamtan <cmd@Macbooks-MacBook-Pro.local>
2026-05-13 17:20:02 -0400

    Fix: remove terser, use esbuild (default minifier)

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,6 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    },
     rollupOptions: {
       output: {
         manualChunks: {
