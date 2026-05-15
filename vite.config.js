18388d8e960675e681feb5ed6d5267fcf02b042c
Dariush Tahamtan <cmd@Macbooks-MacBook-Pro.local>
2026-05-15 12:17:55 -0400

    fix: move assets to static/ dir to bypass CF cached wildcard redirect

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cssInjectedByJsPlugin()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
+    assetsDir: 'static',
     sourcemap: false,
+    cssCodeSplit: false,
     rollupOptions: {
       output: {
         manualChunks: {
